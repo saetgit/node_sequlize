148af0c43f178e1ebdebeef76a53d374bba69796
Sara Etemadian <s.etemadian@ub.ac>
2021-03-16 02:37:30 -0700

    addcontrollers and other folder

---
+++ controllers/UserControllers.js
@@ -0,0 +1,16 @@
+const User=require('../models/user');
+
+module.exports={
+    async index(req,res){
+
+    },
+    async store(req,res){
+
+    },
+    async update(req,res){
+
+    },
+    async delete(req,res){
+
+    },
+};
